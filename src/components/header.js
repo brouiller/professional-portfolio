f3c67bd2a5c181502a6e6f3a258d4c58fb8166fb
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-11-24 09:28:38 -0500

    added updated resume and fixed link

--- src/components/header.js
+++ src/components/header.js
@@ -2,29 +2,29 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <header className="container-fluid">
-            <div className="row">
-                <h1 className="col-6 display-1">Bradley Kimbrell</h1>
-                <nav className="navbar navbar-dark col-5">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="#aboutme">
-                            <span className="rounded p-3">About Me</span>
-                        </a>
-                        <a className="navbar-brand" href="#work">
-                            <span className="rounded p-3">Work</span>
-                        </a>
-                        <a className="navbar-brand" href="#contactme">
-                            <span className="rounded p-3">Contact Me</span>
-                        </a>
-                        <a
-                            className="navbar-brand"
-                            href="./assets/documents/BradleyKimbrellFullStackResume.pdf"
-                        >
-                            <span className="rounded p-3">Resume</span>
-                        </a>
-                    </div>
-                </nav>
+      <header className="container-fluid">
+        <div className="row">
+          <h1 className="col-6 display-1">Bradley Kimbrell</h1>
+          <nav className="navbar navbar-dark col-5">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#aboutme">
+                <span className="rounded p-3">About Me</span>
+              </a>
+              <a className="navbar-brand" href="#work">
+                <span className="rounded p-3">Work</span>
+              </a>
+              <a className="navbar-brand" href="#contactme">
+                <span className="rounded p-3">Contact Me</span>
+              </a>
+              <a
+                className="navbar-brand"
+                href="../images/Bradley-Kimbrell-Resume-2021.pdf"
+              >
+                <span className="rounded p-3">Resume</span>
+              </a>
             </div>
-        </header>
-    )
+          </nav>
+        </div>
+      </header>
+    );
 }
